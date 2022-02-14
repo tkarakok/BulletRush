e70e1c6ec7ec36762d9ec6132e0a86ed19993018
Tunahan <59062912+tkarakok@users.noreply.github.com>
2022-02-14 16:48:12 +0300

    obstacle system added

--- Bullet Rush/Assets/Scripts/Bullet/Collect.cs
+++ Bullet Rush/Assets/Scripts/Bullet/Collect.cs
@@ -11,10 +11,21 @@ public class Collect : MonoBehaviour
         if (other.CompareTag("Collect"))
         {
             other.tag = "Untagged";
+            GameManager.Instance.bullets.Add(other.gameObject);
             other.transform.SetParent(parent);
-            other.gameObject.transform.position = transform.position + new Vector3(.6f, 0, .6f);
+            other.gameObject.transform.position = GameManager.Instance.bullets[GameManager.Instance.bullets.Count - 1].transform.position + new Vector3(.6f, 0, .6f);
             other.gameObject.AddComponent<Collect>().parent = parent;
             EventManager.Instance.CollectBullet();
+        }
+        else if (other.CompareTag("Wall"))
+        {
+            GameManager.Instance.bullets.Remove(gameObject);
+            transform.SetParent(null);
+            gameObject.SetActive(false);
+            if (GameManager.Instance.bullets.Count == 0)
+            {
+                Debug.Log("game over");
+            }
         }
     }
 
--- Bullet Rush/Assets/Scripts/Game/GameManager.cs
+++ Bullet Rush/Assets/Scripts/Game/GameManager.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    public List<GameObject> bullets;
+
 }
